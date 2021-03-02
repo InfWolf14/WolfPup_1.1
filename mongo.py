bee9fbf2e3225021bcd1b1190aea58156e3b6b1b
InfWolf14 <infsutter14@gmail.com>
2021-03-02 00:44:18 -0500

    Update mongo.py

--- mongo.py
+++ mongo.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 class Mongo(commands.Cog, name='Mongo'):
     def __init__(self, bot):
         self.bot = bot
-        self.db = MongoClient('mongodb+srv://Gigi-Bot:t48L477c42EpiHUG@gigidb.rztdf.mongodb.net/?retryWrites=true&w=majority')
+        self.db = MongoClient('mongodb+srv://Gigi-Bot:<password>@gigidb.rztdf.mongodb.net/?retryWrites=true&w=majority')
 
     def init_db(self):
         return self.db
