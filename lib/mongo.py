019182b124a9c54bc0e628c2d391b02316d6a378
InfWolf14 <infsutter14@gmail.com>
2021-03-08 19:00:46 -0500

    Push new db lib structure

--- lib/mongo.py
+++ lib/mongo.py
@@ -1,15 +1,9 @@
 from pymongo import MongoClient
-from discord.ext import commands
 
 
-class Mongo(commands.Cog, name='Mongo'):
-    def __init__(self, bot):
-        self.bot = bot
-        self.db = MongoClient('mongodb+srv://Gigi-Bot:t48L477c42EpiHUG@gigidb.rztdf.mongodb.net/?retryWrites=true&w=majority')
+class Mongo:
+    def __init__(self):
+        self.db = MongoClient('mongodb+srv://Gigi-Bot:<password>@gigidb.rztdf.mongodb.net/?retryWrites=true&w=majority')
 
     def init_db(self):
         return self.db
-
-
-def setup(bot):
-    bot.add_cog(Mongo(bot))
