2aa36f57818df238ef143570681823d3f6b7263a
Adam_N <naismith.adam@gmail.com>
2021-05-24 21:28:29 -0600

    Bug Smashing

--- cogs/leaderboard.py
+++ cogs/leaderboard.py
@@ -15,54 +15,55 @@ class Leaderboard(commands.Cog):
 
     @commands.command(name='leaderboard', aliases=['lb'])
     async def leaderboard(self, ctx, *args):
-        await ctx.message.delete()
-        self.server_db = self.db[str(ctx.guild.id)]['users']
-        leaderboard = None
-        if args:
-            leaderboard = ' '.join(args)
-        if await Util.check_channel(ctx):
-            new_embed = discord.Embed(title=f':trophy: __**User Leaderboards**__ :trophy:',
-                                      color=discord.Colour.gold())
-            new_embed.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
-            new_embed.set_footer(text=f'Results provided by: {self.bot.user.name}', icon_url=self.bot.user.avatar_url)
-            for stat in self.leaderboards:
-                if leaderboard is not None:
-                    if leaderboard in self.leaderboards[stat]:
-                        leaderboard = stat
-                    else:
-                        continue
-                users = self.server_db.find().sort(stat, -1)
-                rank = 1
-                listing, list_user, f_name, user_str = '', '', '', ''
-                stat_1, stat_2, my_stat = None, None, None
-                for user in users:
-                    try:
-                        list_user = self.bot.get_user(int(user["_id"]))
-
-                        try:
-                            stat_1, stat_2 = stat.split('.')
-                        except ValueError:
-                            pass
-                        if stat_2 is not None:
-                            f_name = f'**{stat_2.capitalize().replace("_", " ")}**'
-                            user_str = f'`[{str(rank)}.]` *{list_user.name} :* **{user[stat_1][stat_2]}**'
+        async with ctx.channel.typing():
+            await ctx.message.delete()
+            self.server_db = self.db[str(ctx.guild.id)]['users']
+            leaderboard = None
+            if args:
+                leaderboard = ' '.join(args)
+            if await Util.check_channel(ctx):
+                new_embed = discord.Embed(title=f':trophy: __**User Leaderboards**__ :trophy:',
+                                          color=discord.Colour.gold())
+                new_embed.set_author(name=ctx.guild.name, icon_url=ctx.guild.icon_url)
+                new_embed.set_footer(text=f'Results provided by: {self.bot.user.name}', icon_url=self.bot.user.avatar_url)
+                for stat in self.leaderboards:
+                    if leaderboard is not None:
+                        if leaderboard in self.leaderboards[stat]:
+                            leaderboard = stat
                         else:
-                            f_name = f'**{stat.capitalize()}**'
-                            user_str = f'`[{str(rank)}.]` *{list_user.name} :* **{user[stat]}**'
-                        if list_user == ctx.author:
-                            my_stat = user_str
-                            user_str = f'**\u27A4** {user_str} '
-                        if rank <= 15:
-                            listing += f'{user_str}\n'
-                            rank += 1
-                    except AttributeError:
-                        continue
-                if my_stat is not None:
-                    listing += f'\n__**Your Ranking:**__\n{my_stat}'
-                new_embed.add_field(name=f_name,
-                                    value=listing,
-                                    inline=True)
-            await ctx.send(embed=new_embed)
+                            continue
+                    users = self.server_db.find().sort(stat, -1)
+                    rank = 1
+                    listing, list_user, f_name, user_str = '', '', '', ''
+                    stat_1, stat_2, my_stat = None, None, None
+                    for user in users:
+                        try:
+                            list_user = self.bot.get_user(int(user["_id"]))
+
+                            try:
+                                stat_1, stat_2 = stat.split('.')
+                            except ValueError:
+                                pass
+                            if stat_2 is not None:
+                                f_name = f'**{stat_2.capitalize().replace("_", " ")}**'
+                                user_str = f'`[{str(rank)}.]` *{list_user.name} :* **{user[stat_1][stat_2]}**'
+                            else:
+                                f_name = f'**{stat.capitalize()}**'
+                                user_str = f'`[{str(rank)}.]` *{list_user.name} :* **{user[stat]}**'
+                            if list_user == ctx.author:
+                                my_stat = user_str
+                                user_str = f'**\u27A4** {user_str} '
+                            if rank <= 15:
+                                listing += f'{user_str}\n'
+                                rank += 1
+                        except AttributeError:
+                            continue
+                    if my_stat is not None:
+                        listing += f'\n__**Your Ranking:**__\n{my_stat}'
+                    new_embed.add_field(name=f_name,
+                                        value=listing,
+                                        inline=True)
+                await ctx.send(embed=new_embed)
 
 
 def setup(bot):
--- cogs/profile.py
+++ cogs/profile.py
@@ -86,29 +86,30 @@ class Profile(commands.Cog):
     @commands.command(name='search')
     async def search(self, ctx, query, exact_match: bool = False):
         """Search for a user"""
-        await ctx.message.delete()
-        self.server_db = self.db[str(ctx.guild.id)]['users']
-        results = []
-        count = 0
-        if await Util.check_channel(ctx):
-            user = self.server_db.find()
-            for _, user_data in enumerate(user):
-                for platform in self.sys_aliases:
-                    username = user_data['profile']['aliases'][platform]
-                    if username is not None:
-                        if not exact_match:
-                            query = query.lower()
-                            username = username.lower()
-                        if query in username and user_data['_id'] not in results:
-                            count += 1
-                            member = await ctx.guild.fetch_member(int(user_data['_id']))
-                            results.append(f'`[{str(count)}.]` {member.mention}')
-            if len(results) > 0:
-                await ctx.send(embed=discord.Embed(title=f'Found {len(results)} match(es):',
-                                                   description='\n'.join(results),
-                                                   color=discord.Colour.gold()))
-            else:
-                await ctx.send(embed=discord.Embed(title=f'No Matches Found'))
+        async with ctx.channel.typing():
+            await ctx.message.delete()
+            self.server_db = self.db[str(ctx.guild.id)]['users']
+            results = []
+            count = 0
+            if await Util.check_channel(ctx):
+                user = self.server_db.find()
+                for _, user_data in enumerate(user):
+                    for platform in self.sys_aliases:
+                        username = user_data['profile']['aliases'][platform]
+                        if username is not None:
+                            if not exact_match:
+                                query = query.lower()
+                                username = username.lower()
+                            if query in username and user_data['_id'] not in results:
+                                count += 1
+                                member = await ctx.guild.fetch_member(int(user_data['_id']))
+                                results.append(f'`[{str(count)}.]` {member.mention}')
+                if len(results) > 0:
+                    await ctx.send(embed=discord.Embed(title=f'Found {len(results)} match(es):',
+                                                       description='\n'.join(results),
+                                                       color=discord.Colour.gold()))
+                else:
+                    await ctx.send(embed=discord.Embed(title=f'No Matches Found'))
 
     @commands.command(name='delete', aliases=['del'])
     async def delete(self, ctx, system):
@@ -131,117 +132,118 @@ class Profile(commands.Cog):
     @commands.command(aliases=['card', 'profilecard', 'canvas'])
     async def profile(self, ctx, member: discord.Member = None):
         """Displays your profile card."""
-        self.server_db = self.db[str(ctx.guild.id)]['users']
-        print('in')
-        if await Util.check_channel(ctx):
-            if member is None:
-                member = ctx.author
-            async with ctx.message.channel.typing():
-                user = self.server_db.find_one({'_id': str(member.id)})
-                bg_img = Image.open('assets/card_to_draw.png').convert('RGBA')
-                ref_img = bg_img.copy()
-                draw = ImageDraw.Draw(ref_img)
-                title_font = ImageFont.truetype('assets/font/NASHVILL.TTF', size=42)
-                text_font = ImageFont.truetype('assets/font/HELLDORA.TTF', size=22)
-                alias_font = ImageFont.truetype('assets/font/libel_suit.ttf', size=20)
-                buffer = 20
-                draw_bounds = [buffer, (ref_img.width-buffer), buffer, (ref_img.height-buffer)]
-                ref_coord = [draw_bounds[0], draw_bounds[2]]
-                # # Title Draw # #
-                text_size = draw.textsize(text='WANTED', font=title_font)
-                draw.text((int((ref_img.width/2)-(text_size[0]/2)), ref_coord[1]),
-                          text='WANTED', fill=(16, 16, 16), font=title_font)
-                draw.line((ref_coord[0], ref_coord[1]+int(text_size[1]/2),
-                           int(ref_img.width/2)-int(text_size[0]/2+int(buffer/2)), ref_coord[1]+int(text_size[1]/2)),
-                          fill=(16, 16, 16), width=5)
-                draw.line((int(ref_img.width/2)+int(text_size[0]/2+int(buffer/2)), ref_coord[1]+int(text_size[1]/2),
-                           draw_bounds[1], ref_coord[1]+int(text_size[1]/2)),
-                          fill=(16, 16, 16), width=5)
-                draw.line((draw_bounds[0]+buffer, ref_coord[1]+(text_size[1]+int(buffer/2)),
-                           draw_bounds[1]-buffer, ref_coord[1]+(text_size[1]+int(buffer/2))),
-                          fill=(16, 16, 16), width=5)
-                ref_coord[0] += int(buffer/2)
-                ref_coord[1] += int(text_size[1]+int(buffer*1.5))
-                # # Avatar Draw # #
-                avatar_img = Image.open('assets/gigi_avatar.png')
-                if member.avatar:
-                    avatar_asset = member.avatar_url_as(format='png', size=128)
-                    buffer_img = io.BytesIO()
-                    await avatar_asset.save(buffer_img)
-                    buffer_img.seek(0)
-                    avatar_img = Image.open(buffer_img).resize((128, 128)).convert('RGBA')
-                emblem_img = Image.open(f'assets/emblem_{user["level"]}.png').convert('RGBA')
-                draw.rectangle((ref_coord[0]-5, ref_coord[1]-5,
-                                ref_coord[0]+avatar_img.width+5, ref_coord[1]+avatar_img.height+5),
-                               fill=(16, 16, 16), width=5)
-                ref_img.paste(avatar_img, (ref_coord[0], ref_coord[1]), avatar_img)
-                ref_img.paste(emblem_img, (int(ref_coord[0]+int(avatar_img.width/2)-int(emblem_img.width/2)),
-                                           int(ref_coord[1]+avatar_img.height+1)), emblem_img)
-                ref_coord[0] += avatar_img.width+buffer
-                # # Level/Exp Draw # #
-                x_anchor = ref_coord[0]
-                draw.line((ref_coord[0]-buffer, ref_coord[1]+buffer,
-                           draw_bounds[1]-buffer, ref_coord[1]+buffer),
-                          fill=(16, 16, 16), width=5)
-                text_size = draw.textsize(text=f'LVL: {user["level"]}', font=text_font)
-                draw.text((ref_coord[0], ref_coord[1]-int(text_size[1]/2)+1),
-                          text=f'LVL: {user["level"]}', fill=(32, 32, 32), font=text_font)
-                draw.line((ref_coord[0]+text_size[0]+int(buffer/2), ref_coord[1]-buffer,
-                           ref_coord[0]+text_size[0]+int(buffer/2), ref_coord[1]+buffer),
-                          fill=(16, 16, 16), width=3)
-                ref_coord[0] += text_size[0]+buffer
-                text_size = draw.textsize(text=f'Bounty: ${user["exp"]}', font=text_font)
-                draw.text((ref_coord[0], ref_coord[1]-int(text_size[1]/2)+1),
-                          text=f'Bounty: ${user["exp"]}', fill=(32, 32, 32), font=text_font)
-                # # Alias Draw # #
-                ref_coord[0] = x_anchor
-                ref_coord[1] += int(buffer*1.5)
-                for platform in user['profile']['aliases']:
-                    if user['profile']['aliases'][platform] is not None:
-                        username = user['profile']['aliases'][platform]
-                        if len(username) > 15:
-                            try:
-                                first, second = username.split(' ')
-                                username = first+'\n'+second
-                            except ValueError:
-                                pass
-                        icon_img = Image.open(f'assets/icon/{platform}_logo.png').resize((24, 24)).convert('RGBA')
-                        text_size = draw.textsize(text=f'{username}', font=alias_font)
-                        sample = icon_img.width+int(buffer/2)+text_size[0]+buffer
-                        while sample >= (draw_bounds[1]-x_anchor)/2 or text_size[1] > icon_img.height+5:
-                            alias_font = ImageFont.truetype('assets/font/libel_suit.ttf', size=(alias_font.size-1))
-                            text_size = draw.multiline_textsize(text=f'{username}', font=alias_font, spacing=1)
+        async with ctx.channel.typing():
+            self.server_db = self.db[str(ctx.guild.id)]['users']
+            print('in')
+            if await Util.check_channel(ctx):
+                if member is None:
+                    member = ctx.author
+                async with ctx.message.channel.typing():
+                    user = self.server_db.find_one({'_id': str(member.id)})
+                    bg_img = Image.open('assets/card_to_draw.png').convert('RGBA')
+                    ref_img = bg_img.copy()
+                    draw = ImageDraw.Draw(ref_img)
+                    title_font = ImageFont.truetype('assets/font/NASHVILL.TTF', size=42)
+                    text_font = ImageFont.truetype('assets/font/HELLDORA.TTF', size=22)
+                    alias_font = ImageFont.truetype('assets/font/libel_suit.ttf', size=20)
+                    buffer = 20
+                    draw_bounds = [buffer, (ref_img.width-buffer), buffer, (ref_img.height-buffer)]
+                    ref_coord = [draw_bounds[0], draw_bounds[2]]
+                    # # Title Draw # #
+                    text_size = draw.textsize(text='WANTED', font=title_font)
+                    draw.text((int((ref_img.width/2)-(text_size[0]/2)), ref_coord[1]),
+                              text='WANTED', fill=(16, 16, 16), font=title_font)
+                    draw.line((ref_coord[0], ref_coord[1]+int(text_size[1]/2),
+                               int(ref_img.width/2)-int(text_size[0]/2+int(buffer/2)), ref_coord[1]+int(text_size[1]/2)),
+                              fill=(16, 16, 16), width=5)
+                    draw.line((int(ref_img.width/2)+int(text_size[0]/2+int(buffer/2)), ref_coord[1]+int(text_size[1]/2),
+                               draw_bounds[1], ref_coord[1]+int(text_size[1]/2)),
+                              fill=(16, 16, 16), width=5)
+                    draw.line((draw_bounds[0]+buffer, ref_coord[1]+(text_size[1]+int(buffer/2)),
+                               draw_bounds[1]-buffer, ref_coord[1]+(text_size[1]+int(buffer/2))),
+                              fill=(16, 16, 16), width=5)
+                    ref_coord[0] += int(buffer/2)
+                    ref_coord[1] += int(text_size[1]+int(buffer*1.5))
+                    # # Avatar Draw # #
+                    avatar_img = Image.open('assets/gigi_avatar.png')
+                    if member.avatar:
+                        avatar_asset = member.avatar_url_as(format='png', size=128)
+                        buffer_img = io.BytesIO()
+                        await avatar_asset.save(buffer_img)
+                        buffer_img.seek(0)
+                        avatar_img = Image.open(buffer_img).resize((128, 128)).convert('RGBA')
+                    emblem_img = Image.open(f'assets/emblem_{user["level"]}.png').convert('RGBA')
+                    draw.rectangle((ref_coord[0]-5, ref_coord[1]-5,
+                                    ref_coord[0]+avatar_img.width+5, ref_coord[1]+avatar_img.height+5),
+                                   fill=(16, 16, 16), width=5)
+                    ref_img.paste(avatar_img, (ref_coord[0], ref_coord[1]), avatar_img)
+                    ref_img.paste(emblem_img, (int(ref_coord[0]+int(avatar_img.width/2)-int(emblem_img.width/2)),
+                                               int(ref_coord[1]+avatar_img.height+1)), emblem_img)
+                    ref_coord[0] += avatar_img.width+buffer
+                    # # Level/Exp Draw # #
+                    x_anchor = ref_coord[0]
+                    draw.line((ref_coord[0]-buffer, ref_coord[1]+buffer,
+                               draw_bounds[1]-buffer, ref_coord[1]+buffer),
+                              fill=(16, 16, 16), width=5)
+                    text_size = draw.textsize(text=f'LVL: {user["level"]}', font=text_font)
+                    draw.text((ref_coord[0], ref_coord[1]-int(text_size[1]/2)+1),
+                              text=f'LVL: {user["level"]}', fill=(32, 32, 32), font=text_font)
+                    draw.line((ref_coord[0]+text_size[0]+int(buffer/2), ref_coord[1]-buffer,
+                               ref_coord[0]+text_size[0]+int(buffer/2), ref_coord[1]+buffer),
+                              fill=(16, 16, 16), width=3)
+                    ref_coord[0] += text_size[0]+buffer
+                    text_size = draw.textsize(text=f'Bounty: ${user["exp"]}', font=text_font)
+                    draw.text((ref_coord[0], ref_coord[1]-int(text_size[1]/2)+1),
+                              text=f'Bounty: ${user["exp"]}', fill=(32, 32, 32), font=text_font)
+                    # # Alias Draw # #
+                    ref_coord[0] = x_anchor
+                    ref_coord[1] += int(buffer*1.5)
+                    for platform in user['profile']['aliases']:
+                        if user['profile']['aliases'][platform] is not None:
+                            username = user['profile']['aliases'][platform]
+                            if len(username) > 15:
+                                try:
+                                    first, second = username.split(' ')
+                                    username = first+'\n'+second
+                                except ValueError:
+                                    pass
+                            icon_img = Image.open(f'assets/icon/{platform}_logo.png').resize((24, 24)).convert('RGBA')
+                            text_size = draw.textsize(text=f'{username}', font=alias_font)
                             sample = icon_img.width+int(buffer/2)+text_size[0]+buffer
-                        if (ref_coord[0] + sample) > draw_bounds[1]:
-                            ref_coord[0] = x_anchor
-                            ref_coord[1] += icon_img.height+int(buffer/2)
-                        ref_img.paste(icon_img, (ref_coord[0], ref_coord[1]), icon_img)
-                        draw.multiline_text((ref_coord[0]+icon_img.width+int(buffer/2), ref_coord[1]-(text_size[1]/2-(icon_img.height/2))),
-                                            text=f'{username}', fill=(32, 32, 32), font=alias_font, spacing=1)
-                        ref_coord[0] += 117
-                ref_coord[0] = draw_bounds[0]
-                ref_coord[1] = draw_bounds[3]-int(buffer*1.5)+1
-                # # Draw Wanted Text # #
-                text_size = draw.textsize(text='Wanted for', font=text_font)
-                draw.line((ref_coord[0], ref_coord[1], ref_coord[0]+buffer, ref_coord[1]),
-                          fill=(16, 16, 16), width=5)
-                draw.text((ref_coord[0]+buffer+2, ref_coord[1]-(text_size[1]/2)),
-                          text='Wanted for', fill=(16, 16, 16), font=text_font)
-                draw.line((ref_coord[0]+text_size[0]+buffer+1, ref_coord[1], draw_bounds[1]-(buffer*2.5), ref_coord[1]),
-                          fill=(16, 16, 16), width=5)
-                wanted_text = user['profile']['wanted_text']
-                if wanted_text is None:
-                    wanted_text = 'Shootin\', lootin\', and rootin\' tootin\' degeneracy'
-                text_font = ImageFont.truetype('assets/font/HELLDORA.TTF', size=17)
-                text_size = draw.textsize(text=wanted_text, font=text_font)
-                draw.text((ref_coord[0]+int(buffer/4), ref_coord[1]+text_size[1]-2),
-                          text=wanted_text, fill=(48, 48, 48), font=text_font)
-                # Send finished image
-                send_buffer = io.BytesIO()
-                ref_img.save(send_buffer, format='PNG')
-                send_buffer.seek(0)
-                await ctx.message.delete()
-                await ctx.send(file=File(send_buffer, 'myimage.png'))
+                            while sample >= (draw_bounds[1]-x_anchor)/2 or text_size[1] > icon_img.height+5:
+                                alias_font = ImageFont.truetype('assets/font/libel_suit.ttf', size=(alias_font.size-1))
+                                text_size = draw.multiline_textsize(text=f'{username}', font=alias_font, spacing=1)
+                                sample = icon_img.width+int(buffer/2)+text_size[0]+buffer
+                            if (ref_coord[0] + sample) > draw_bounds[1]:
+                                ref_coord[0] = x_anchor
+                                ref_coord[1] += icon_img.height+int(buffer/2)
+                            ref_img.paste(icon_img, (ref_coord[0], ref_coord[1]), icon_img)
+                            draw.multiline_text((ref_coord[0]+icon_img.width+int(buffer/2), ref_coord[1]-(text_size[1]/2-(icon_img.height/2))),
+                                                text=f'{username}', fill=(32, 32, 32), font=alias_font, spacing=1)
+                            ref_coord[0] += 117
+                    ref_coord[0] = draw_bounds[0]
+                    ref_coord[1] = draw_bounds[3]-int(buffer*1.5)+1
+                    # # Draw Wanted Text # #
+                    text_size = draw.textsize(text='Wanted for', font=text_font)
+                    draw.line((ref_coord[0], ref_coord[1], ref_coord[0]+buffer, ref_coord[1]),
+                              fill=(16, 16, 16), width=5)
+                    draw.text((ref_coord[0]+buffer+2, ref_coord[1]-(text_size[1]/2)),
+                              text='Wanted for', fill=(16, 16, 16), font=text_font)
+                    draw.line((ref_coord[0]+text_size[0]+buffer+1, ref_coord[1], draw_bounds[1]-(buffer*2.5), ref_coord[1]),
+                              fill=(16, 16, 16), width=5)
+                    wanted_text = user['profile']['wanted_text']
+                    if wanted_text is None:
+                        wanted_text = 'Shootin\', lootin\', and rootin\' tootin\' degeneracy'
+                    text_font = ImageFont.truetype('assets/font/HELLDORA.TTF', size=17)
+                    text_size = draw.textsize(text=wanted_text, font=text_font)
+                    draw.text((ref_coord[0]+int(buffer/4), ref_coord[1]+text_size[1]-2),
+                              text=wanted_text, fill=(48, 48, 48), font=text_font)
+                    # Send finished image
+                    send_buffer = io.BytesIO()
+                    ref_img.save(send_buffer, format='PNG')
+                    send_buffer.seek(0)
+                    await ctx.message.delete()
+                    await ctx.send(file=File(send_buffer, 'myimage.png'))
 
     @commands.command(name='wanted')
     async def wanted(self, ctx, *text):
